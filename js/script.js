00c35635f63f1b7257c58e749c35ce8d18fc22dd
Daria <101666130+DariaChernomorets@users.noreply.github.com>
2022-09-17 15:05:25 +0300

    with remarks

--- js/script.js
+++ js/script.js
@@ -1,35 +1,23 @@
+'use strict';
+
 (function () {
-    'use strict'
-    const random = (max) => {
-        let usedNumArray = [];
-
-        return function getRandom() {
-            const randomNum = Math.floor(Math.random() * max) + 1;
-            if (usedNumArray.length === max) usedNumArray = [];
-
-            if (usedNumArray.includes(randomNum)) {
-                return getRandom()
-            } else {
-                usedNumArray.push(randomNum)
-                return randomNum;
-            }
-        }
-    }
-
 
     const createTable = (col,row) => {
 
         const table = document.createElement('table');
         table.style.border = "solid 2px black";
 
-        for (let i = 0; i <= row; i++){
+        let count = 0;
+
+        for (let i = 0; i < row; i++){
             const tableRow = document.createElement('tr')
             tableRow.style.border = "solid 2px black";
 
-            for (let j = 0; j<=col; j++){
+            for (let j = 0; j<col; j++){
                 const tableCol = document.createElement('td')
-                const num = random(100);
-                tableCol.innerHTML = num()
+
+                count +=1
+                tableCol.innerHTML= `${count}`;
                 tableRow.append(tableCol)
 
                 tableCol.style.border = "solid 2px black";
@@ -39,14 +27,13 @@
             }
             table.append(tableRow)
 
+
         }
         return table
 
     }
 
-    const newTable = createTable(55,15);
+    const newTable = createTable(10,10);
 
     document.body.append(newTable)
-
 })()
-
